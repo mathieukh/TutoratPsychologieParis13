514836775a4d7c9571481a4ba5a83bdc1f841ff3
Mathieu Khalem <mathieu.khalem@etu.upmc.fr>
2017-11-20 00:57:27 +0100

    Création du projet de l'application à destination du Tutorat de Psychologie de Paris 13

---
+++ app/src/main/java/com/mathieukh/tutopsychop13/MainActivity.kt
@@ -0,0 +1,9 @@
+package com.mathieukh.tutopsychop13
+
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * Créé par Mathieu KHALEM le 20/11/2017
+ * Activité principale de l'application
+ */
+class MainActivity : AppCompatActivity()
