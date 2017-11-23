283eccdd4210ba0108ae0c9e2c135c65fa817c1c
Mathieu Khalem <mathieu.khalem@etu.upmc.fr>
2017-11-23 19:52:03 +0100

    Ajout de la première activité vide. Nouveaux imports nécessaires
    Signed-off-by: Mathieu Khalem <mathieu.khalem@etu.upmc.fr>

--- app/src/main/java/com/mathieukh/tutopsychop13/MainActivity.kt
+++ app/src/main/java/com/mathieukh/tutopsychop13/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.mathieukh.tutopsychop13
 
+import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 
-/**
- * Créé par Mathieu KHALEM le 20/11/2017
- * Activité principale de l'application
- */
-class MainActivity : AppCompatActivity()
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
